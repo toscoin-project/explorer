46909993fec6a7b2bc38a244ec214656c3613fef
Bryan G. Lee <bryanlee@tosblock.com>
2018-03-21 17:21:52 +0900

    added network views;added network settings;added peers script;

--- scripts/peers.js
+++ scripts/peers.js
@@ -23,7 +23,7 @@ mongoose.connect(dbString, function(err) {
     console.log('Aborting');
     exit();
   } else {
-    request({uri: 'http://127.0.0.1:' + settings.port + '/api/getpeerinfo', json: true}, function (error, response, body) {
+    request({uri: 'http://ex.tosblock.com/api/getpeerinfo', json: true}, function (error, response, body) {
       lib.syncLoop(body.length, function (loop) {
         var i = loop.iteration();
         var address = body[i].addr.split(':')[0];
